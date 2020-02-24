a7a924d648ea49b1b1226a8a10a12c7e59072743
soroushshalali <soroushshalali@gmail.com>
2020-02-24 20:25:54 +0100

    v0.5

---
+++ assets/js/main-shop.js
@@ -0,0 +1,16 @@
+
+
+function showsidebar(){
+    document.getElementById("sidebar").style.width="30%";
+    document.getElementById("sidebar").style.height="40vh";
+    document.getElementById("btn1").style.display="none";
+    document.getElementById("btn2").style.display="inline-block";
+}
+
+function hidesidebar(){
+    document.getElementById("sidebar").style.width="0%";
+    document.getElementById("sidebar").style.height="0vh";
+    document.getElementById("btn1").style.display="inline-block";
+    document.getElementById("btn2").style.display="none";
+    
+}
